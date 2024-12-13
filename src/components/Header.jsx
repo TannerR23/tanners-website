7997af7b3fb9409b4499ca53765c84b535978080
tr191 <tr191@students.waikato.ac.nz>
2024-12-14 00:23:53 +1300

    Fixed issue with trying to select home icon while mobile nav open

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -36,7 +36,7 @@ const Header = () => {
       }`}
     >
       <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
-        <a className="lg:absolute block w-[12rem] xl:mr-8" href="#home">
+        <a className="lg:absolute block w-[12rem] xl:mr-8" href="#home" onClick={handleClick}>
           <img
             src={home_logo}
             width={190}
