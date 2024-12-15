ee68534486e6d21a32a65641459a18ff4181e7f2
tr191 <tr191@students.waikato.ac.nz>
2024-12-15 17:32:01 +1300

    Updated config to support secondary page for received email

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/tanners-website',
+  base: '/tanners-website/',
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        receivedEmail: 'receivedEmail.html'
+      }
+    }
+  }
 })
